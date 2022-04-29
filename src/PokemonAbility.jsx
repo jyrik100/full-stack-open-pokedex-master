c37fb43852c79c0711c093bfd4b4306db205b744
jyrik100 <104065854+jyrik100@users.noreply.github.com>
2022-04-29 12:57:42 +0300

    NewChanges

---
+++ src/PokemonAbility.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PokemonAbility = ({ abilityName }) => (
+  <div className="pokemon-ability">
+    <div className="pokemon-ability-type">Hidden adadbility</div>
+    <div className="pokemon-ability-name">
+      {abilityName}
+    </div>
+  </div>
+)
+
+export default PokemonAbility
