da492c19b8a48eb6ae6edcb0028f239aaf429799
jyrik100 <104065854+jyrik100@users.noreply.github.com>
2022-04-29 15:24:53 +0300

    commit2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a portadasdaadasdddd
+// Heroku dynamically sets a portadasdaadasddddasdasdasd
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
