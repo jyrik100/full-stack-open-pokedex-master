5fcca6d651b83f0cc0314e95998c8d488497acfb
jyrik100 <104065854+jyrik100@users.noreply.github.com>
2022-05-02 13:10:38 +0300

    newTest

--- cypress/integration/e2e1.js
+++ cypress/integration/e2e1.js
@@ -4,4 +4,23 @@ describe('Pokedex', function() {
       cy.contains('ivysaur')
       cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
     })
+    it('item can be clicked', function() {
+      cy.contains('ivysaur').click()
+      cy.contains('chlorophyll')      
+    })  
   })
+
+
+/*   it('item can be clicked', function() {
+    cy.contains('login').click()
+    cy.get('input:first').type('mluukkai')
+    cy.get('input:last').type('salainen')    
+    cy.get('#login-button').click()
+    cy.contains('new Blog').click()      
+    cy.get('#input1').type('uusi blogi')      
+    cy.contains('save').click()      
+    cy.contains('uusi blogi')
+  })  
+
+
+ */
